6687a63892e96a9f2c292aa9ffeb997dcff1b55e
Jeni Andriawan <jeniandriawan18@gmail.com>
2024-10-01 20:26:37 +0700

    Add files via upload

---
+++ program latihan potensio.ino
@@ -0,0 +1,25 @@
+// Definisikan pin analog
+const int potPin = 14;  // Gunakan GPIO 14 untuk membaca potensiometer
+
+void setup() {
+  // Inisialisasi serial monitor untuk menampilkan data
+  Serial.begin(115200);
+}
+
+void loop() {
+  // Membaca nilai analog dari potensiometer (range: 0-4095)
+  int potValue = analogRead(potPin);
+  
+  // Konversi nilai ADC ke tegangan (3.3V max untuk ESP32)
+  float voltage = (potValue / 4095.0) * 3.3;
+  
+  // Tampilkan nilai ADC dan tegangan di serial monitor
+  Serial.print("Nilai ADC: ");
+  Serial.print(potValue);
+  Serial.print("\t Tegangan: ");
+  Serial.print(voltage);
+  Serial.println(" V");
+  
+  // Tunggu sebentar sebelum membaca lagi
+  delay(500);
+}
